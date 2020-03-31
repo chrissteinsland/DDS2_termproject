cc8f457042742fade5a0b0ae092cdce4e6c7c90b
Christian <chrissteinsland@gmail.com>
2020-03-31 18:29:15 +0200

    Fixed syntax errors

--- tb/assertions_hdlc.sv
+++ tb/assertions_hdlc.sv
@@ -78,11 +78,13 @@ module assertions_hdlc (
   property RX_SC_correct;
  	@(posedge Clk) disable iff(!Rst) $rose(Rx_EoF) |->
 		if(Rx_AbortSignal)
-			(!Rx_Overflow ##0 Rx_AbortSignal ##0 !Rx_FrameError ##0 !Rx_Ready)	
+			(!Rx_Overflow ##0 Rx_AbortSignal ##0 !Rx_FrameError ##0 !Rx_Ready) $display("Aborted frame received")	
 		else if(Rx_Overflow)
-			(Rx_Overflow ##0 !Rx_AbortSignal ##0 !Rx_FrameError ##0 Rx_Ready)	
+			(Rx_Overflow ##0 !Rx_AbortSignal ##0 !Rx_FrameError ##0 !Rx_Ready) $display("Overflowed frame received")
 		else if(Rx_FrameError)
-			(!Rx_Overflow ##0 Rx_AbortSignal ##0 Rx_FrameError ##0 !Rx_Ready)	
+			(!Rx_Overflow ##0 !Rx_AbortSignal ##0 Rx_FrameError ##0 !Rx_Ready) $display("Error frame received")
+		else
+			(!Rx_Overflow ##0 !Rx_AbortSignal ##0 !Rx_FrameError ##0 Rx_Ready) $display("Normal frame received");
 		end
   endproperty
 
@@ -91,4 +93,7 @@ module assertions_hdlc (
     $error("RX status control register is not correct at time %0t", $time); 
     ErrCntAssertions++; 
    end
+
+
+
 endmodule
