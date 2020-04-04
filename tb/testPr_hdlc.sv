90bc7f71ab29504b336fcbafc5cef6a27b7a5c47
Christian <chrissteinsland@gmail.com>
2020-04-04 18:46:42 +0200

    Fixing problem

--- tb/testPr_hdlc.sv
+++ tb/testPr_hdlc.sv
@@ -360,7 +360,7 @@ program testPr_hdlc(
   endtask
 
 	task Verify_DataOutBuff(logic [127:0][7:0] Data, int Size);
-		for(int i=0;i<Size;i++) begin
+		for(int i=0;i<Size;@(posedge uin_hdlc.Clk) i++) begin
 			bit Skip;
 			@(posedge uin_hdlc.Clk) assert (uin_hdlc.Tx_DataOutBuff == Data[i]) Skip = 0;		
 				else begin
